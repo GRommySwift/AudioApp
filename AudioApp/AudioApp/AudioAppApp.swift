0ebee11734eeb7e52e785d4a9affb40d63a241c2
Roman <romaholovai@gmail.com>
2026-01-06 20:17:00 +0100

    initial commit

---
+++ AudioApp/AudioApp/AudioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioAppApp.swift
+//  AudioApp
+//
+//  Created by Roman on 06/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct AudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
